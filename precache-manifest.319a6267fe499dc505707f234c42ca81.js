3df8fe8fcb3667a75917dddee3bc980f057856ad
deshmukhprasad <deshmukhprasad@users.noreply.github.com>
2020-01-13 05:00:18 +0000

    Deploy

---
+++ precache-manifest.319a6267fe499dc505707f234c42ca81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf69b67deafcc47a7ae3",
+    "url": "css/app.1660392f.css"
+  },
+  {
+    "revision": "cf69b67deafcc47a7ae3",
+    "url": "js/app.c4b7c7b4.js"
+  },
+  {
+    "revision": "c00aa80d34b334474459",
+    "url": "js/chunk-vendors.2d1eee83.js"
+  },
+  {
+    "revision": "8f4a2b2ff8db292ebca32989d82b3140",
+    "url": "index.html"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  }
+];
